ab02468ac359504f6e96f00db17d44c528953f9f
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2022-07-26 11:33:13 -0300

    commit

--- backend_mercadinho/bin/backend_mercadinho.dart
+++ backend_mercadinho/bin/backend_mercadinho.dart
@@ -1,5 +1,51 @@
-import 'package:backend_mercadinho/backend_mercadinho.dart' as backend_mercadinho;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${backend_mercadinho.calculate()}!');
+void main(List<String> arguments) async {
+  final server = await shelf_io.serve(_handle, '0.0.0.0', 8080);
+
+  print('Server online: Port ${server.port}');
 }
+
+Response _handle(Request request) {
+  print(request.url.path);
+
+  if (request.url.path == 'json') {
+    return Response(200, body: json, headers: {
+      'content-type': 'text/json',
+    });
+  }
+
+  if (request.url.path == 'index.html') {
+    return Response(200, body: html, headers: {
+      'content-type': 'text/html',
+    });
+  }
+
+  return Response(404);
+}
+
+final json = '''
+
+{
+  "message": "tudo certo"
+
+}
+
+
+''';
+
+final html = '''
+
+<!DOCTYPE html>
+<html> 
+    <head> 
+        <title>Paulo Mercadinho</title> 
+    </head> 
+    <body> 
+        <h1>Produtos em orfeta</h1> 
+    </body> 
+</html>
+
+
+''';
