bcd5c647c71f4e6f4c9d4830041b0a4f8e75a941
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2022-07-26 10:40:36 -0300

    frist commit

---
+++ backend_mercadinho/bin/backend_mercadinho.dart
@@ -0,0 +1,5 @@
+import 'package:backend_mercadinho/backend_mercadinho.dart' as backend_mercadinho;
+
+void main(List<String> arguments) {
+  print('Hello world: ${backend_mercadinho.calculate()}!');
+}
